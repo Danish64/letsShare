51a8c26d6285c85b6d94c07c264a5ee095a3f989
Danish Ali <danishali0051@gmail.com>
2020-10-18 11:53:20 +0500

    Architecture Settings Started

---
+++ src/Letsshare.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Letsshare = () => {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Lets Share Architecture Settings...</Text>
+    </View>
+  );
+};
+
+export default Letsshare;
