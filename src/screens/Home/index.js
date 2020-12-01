517684f361bceac5db1e8568d3aaf9a3b672e840
Danish Ali <danishali0051@gmail.com>
2020-12-01 18:45:09 +0500

    First-commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -21,7 +21,7 @@ const Component = ({loginRequest, userInfo, navigation}) => {
     loginRequest();
   }, []);
   useEffect(() => {
-    console.log('UserInfo on Home Screen', userInfo);
+    console.log('UserInfo on Home Screen tedt', userInfo);
   }, [userInfo]);
   return (
     <Container>
