f46d9e0058e35dcd44c27f0210565ba333e4cde2
Asad Baig <36822413+AsadBaig039@users.noreply.github.com>
2021-03-02 13:22:24 +0500

    Nearby shares and recommendations

--- src/components/ScreensMainComponents/HomeComponents/NearbyShares/NearbyShares.js
+++ src/components/ScreensMainComponents/HomeComponents/NearbyShares/NearbyShares.js
@@ -3,6 +3,10 @@ import {View} from 'react-native';
 import HorizontalFlatList from '../../../GeneralComponents/HorizontalFlatList';
 import {SectionHeadingText} from 'res/UniversalComponents/Text.js';
 import styles from './style';
+import DRCaseImage from 'res/images/ModulesImages/GeneralImages/myLocation.png';
+import ShareGoods from 'res/images/ModulesImages/GoodsSharingImages/shareGoods.png';
+import ShareSpace from 'res/images/ModulesImages/SpaceSharingImages/selectHome.png';
+
 //Native Exports Ends Here
 //Third Party Exports Starts
 
@@ -10,15 +14,39 @@ import styles from './style';
 
 const Component = () => {
   const data = [
-    {id: '1'},
-    {id: '2'},
-    {id: '3'},
-    {id: '4'},
-    {id: '5'},
-    {id: '6'},
-    {id: '7'},
-    {id: '8'},
-    {id: '9'},
+    {
+      id: '1',
+      title: 'Daily Ride',
+      image: DRCaseImage,
+      price: '200 Rs',
+      priceIcon: 'cash-outline',
+      seats: '3',
+      seatsIcon: 'people-outline',
+      route: 'G11/3,Islamabad to G13/1,Islamabad',
+      routeIcon: 'navigate-outline',
+    },
+    {
+      id: '2',
+      title: 'Goods Share',
+      image: ShareGoods,
+      price: '1000 Rs',
+      priceIcon: 'cash-outline',
+      // seats: '2',
+      // seatsIcon: 'people-outline',
+      route: 'Islamabad to Lahore',
+      routeIcon: 'navigate-outline',
+    },
+    {
+      id: '3',
+      title: 'Space Sharing',
+      image: ShareSpace,
+      price: '5000 Rs/Month',
+      priceIcon: 'cash-outline',
+      // seats: '3',
+      // seatsIcon: 'people-outline',
+      route: 'Warda Hamna Residencia, G11/3, Islamabad ',
+      routeIcon: 'navigate-outline',
+    },
   ];
 
   return (
--- src/components/ScreensMainComponents/HomeComponents/RecommenedShares/RecommendedShares.js
+++ src/components/ScreensMainComponents/HomeComponents/RecommenedShares/RecommendedShares.js
@@ -3,15 +3,54 @@ import {View} from 'react-native';
 import {SectionHeadingText} from 'res/UniversalComponents/Text.js';
 import HorizontalFlatList from '../../../GeneralComponents/HorizontalFlatList';
 import styles from './style';
+import DRCaseImage from 'res/images/ModulesImages/GeneralImages/myLocation.png';
+import ShareGoods from 'res/images/ModulesImages/GoodsSharingImages/shareGoods.png';
+import ShareSpace from 'res/images/ModulesImages/SpaceSharingImages/selectHome.png';
 
 const Component = () => {
+  const data = [
+    {
+      id: '1',
+      title: 'Daily Ride',
+      image: DRCaseImage,
+      price: '200 Rs',
+      priceIcon: 'cash-outline',
+      seats: '3',
+      seatsIcon: 'people-outline',
+      route: 'G11/3,Islamabad to G13/1,Islamabad',
+      routeIcon: 'navigate-outline',
+    },
+    {
+      id: '2',
+      title: 'Goods Share',
+      image: ShareGoods,
+      price: '1000 Rs',
+      priceIcon: 'cash-outline',
+      // seats: '2',
+      // seatsIcon: 'people-outline',
+      route: 'Islamabad to Lahore',
+      routeIcon: 'navigate-outline',
+    },
+    {
+      id: '3',
+      title: 'Space Sharing',
+      image: ShareSpace,
+      price: '5000 Rs/Month',
+      priceIcon: 'cash-outline',
+      // seats: '3',
+      // seatsIcon: 'people-outline',
+      route: 'Warda Hamna Residencia, G11/3, Islamabad ',
+      routeIcon: 'navigate-outline',
+    },
+  ];
+
   return (
     <View style={styles.recommendedArea}>
       <View style={styles.recommendedAreaTitleText}>
         <SectionHeadingText>Recommended Shares</SectionHeadingText>
       </View>
       <View style={styles.recommendedFlatlistArea}>
-        <HorizontalFlatList />
+        <HorizontalFlatList data={data} />
       </View>
     </View>
   );
